63b1bfd1ffeece9126876415178847613c15cbd5
sdkim96 <seong960603@gmail.com>
2026-07-05 22:45:46 +0900

    refactor: move packet types to codec.go, implement   encode, and parse event data from 42 packets

--- socketio/codec.go
+++ socketio/codec.go
@@ -1,6 +1,46 @@
 package socketio
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type EnginePacketType int
+type SocketPacketType int
+
+const (
+	// This None value is a sentinel value for the zero value of EnginePacketType.
+	// It is not a valid EnginePacketType and should not be used in any other context.
+	EngineNone EnginePacketType = iota - 1
+
+	Open
+	Close
+
+	// from server to client
+	Ping
+
+	// from client to server
+	Pong
+	Message
+	Upgrade
+	Noop
+)
+
+const (
+	// This None value is a sentinel value for the zero value of SocketPacketType.
+	// It is not a valid SocketPacketType and should not be used in any other context.
+	SocketNone SocketPacketType = iota - 1
+
+	Connect
+	Disconnect
+
+	Event
+	Ack
+	Error
+
+	BinaryEvent
+	BinaryAck
+)
 
 type Packet struct {
 	EnginePacketType EnginePacketType
@@ -22,23 +62,57 @@ func (p Packet) isEmpty() bool {
 		p.Body == nil)
 }
 
-func (p Packet) event() (string, error) {
-	if p.SocketPacketType == Event {
-		//TODO: The event packet looks like this: 42["event_name",{"key":"value"}]
-		// We need to parse the event name and the data from the body.
-		// For now, we just return the body as a string.
-
-		return string(p.Body), nil
-	}
-	return "", fmt.Errorf("socketio: not an event packet")
+func (p Packet) is40() bool {
+	return p.EnginePacketType == Message && p.SocketPacketType == Connect
 }
 
-func encode(dp Packet) ([]byte, error) {
+func (p Packet) is42() bool {
+	return p.EnginePacketType == Message && p.SocketPacketType == Event
+}
 
-	// TODO: Implement the encoding logic for Socket.IO packets.
-	var p []byte
-	return p, nil
+func (p Packet) is0() bool {
+	return p.EnginePacketType == Open
+}
 
+func (p Packet) event() (string, []byte, error) {
+	if p.is42() {
+		var data []json.RawMessage
+		if err := json.Unmarshal(p.Body, &data); err != nil {
+			return "", nil, fmt.Errorf("socketio: failed to unmarshal event packet body: %w", err)
+		}
+		if len(data) < 1 {
+			return "", nil, fmt.Errorf("socketio: event packet body is empty")
+		}
+		var eventName string
+		if err := json.Unmarshal(data[0], &eventName); err != nil {
+			return "", nil, fmt.Errorf("socketio: failed to unmarshal event name: %w", err)
+		}
+		return eventName, data[1], nil
+	}
+	return "", nil, fmt.Errorf("socketio: not an event packet. Currently: EnginePacketType=%v, SocketPacketType=%v", p.EnginePacketType, p.SocketPacketType)
+}
+
+func encode(p Packet) ([]byte, error) {
+	var b []byte
+	b = append(b, byte(p.EnginePacketType)+'0')
+	switch p.EnginePacketType {
+	case Close, Ping, Pong, Upgrade, Noop:
+		// No SocketPacketType or Body for these types.
+		return b, nil
+	case Open:
+		// Body Only. No SocketPacketType for Open packets.
+		b = append(b, p.Body...)
+		return b, nil
+	case Message:
+		if p.SocketPacketType == SocketNone {
+			return nil, fmt.Errorf("socketio: message packet must have a SocketPacketType")
+		}
+		b = append(b, byte(p.SocketPacketType)+'0')
+		b = append(b, p.Body...)
+		return b, nil
+	default:
+		return nil, fmt.Errorf("socketio: unknown engine packet type: %d", p.EnginePacketType)
+	}
 }
 
 // Decode decodes a Socket.IO packet from the given byte slice.
